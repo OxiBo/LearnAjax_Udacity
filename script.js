3eaddb70179632d407557c3af0ddec6f557225d2
Oksana <xenos2000@ukr.net>
2018-01-16 22:30:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function loadData() {
 
     var $body = $('body');
@@ -20,9 +19,51 @@ function loadData() {
     var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + 
     address + '';
     $body.append('<img class="bgimg" src="' + streetviewURL + '">');
-
+ 
+    var urlForArticls = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ cityStr + '&sort=newest&api-key=b96c642b11034eabb9d2917c95481709';
+    $.getJSON(urlForArticls, function(articles) {
+    console.log('success');
+    })
+    .done(function(articles, textStatus, jqXHR){
+        if(articles.response.docs.length == 0) {
+            $($nytHeaderElem).text('No New York Times Articles About ' + cityStr + ' Today.');
+        }
+        else {
+            $nytHeaderElem.text('New York Times Articles about ' + cityStr); 
+        var allArticles = articles.response.docs;
+        for(var i = 0, n = allArticles.length; i < n; i++){
+            $($nytElem).append('<li class="article"><a href="' + allArticles[i].web_url + '" target="_blank">' + allArticles[i].headline.main + '</a></li>', '<p>' + allArticles[i].snippet + '</p>');
+        }
+    };
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log(errorThrown.toString());
+        $($nytHeaderElem).text('New York Times Articles Could Not Be Loaded');
+    });
 
     return false;
+
 };
 
 $('#form-container').submit(loadData);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
